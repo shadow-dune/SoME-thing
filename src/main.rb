ea29139eabec5f393940eafc532ef7a945ee23e4
zeptoparsec <qwern1177@outlook.com>
2025-06-13 13:58:06 +0530

    new

--- src/main.rb
+++ src/main.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
   "Hello, World!"
 end
+
+put '/poll/:qnum/:ans' do |qn, ans|
+  # record the answer in the database
+end
+
+get '/poll/:qnum/results' do |qn|
+  # % of correct answers
+end
